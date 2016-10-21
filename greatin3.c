ef962d0880dc7b1794b75be1e41d1222a256943b
Rohanlalpradhan <rohan.pradhan76@gmail.com>
2016-10-21 11:15:40 +0545

    Add files via upload

---
+++ greatin3.c
@@ -0,0 +1,33 @@
+/*
+Students Name: Rohan Pradhan
+Roll No.:
+Lab No:5
+Subject: Programming Fundmentals
+Program: greatest among 3
+Date: 21st october
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+int a,b,c;
+
+printf("Enter 3 numbers a,b&c:");
+scanf("%d%d%d",&a,&b,&c);
+
+if(a>b&&a>c)
+	{
+			printf("%d is greatest number",a);
+	}
+
+else if(b>c&&b>a)
+	{
+			printf("%d is greatest number",b);
+	}
+else 
+			printf("%d is the greatest",c);
+
+return 0;
+}
