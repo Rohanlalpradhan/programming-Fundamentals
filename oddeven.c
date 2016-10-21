371f24b7fc632e1af966690873085dcee0f5b571
Rohanlalpradhan <rohan.pradhan76@gmail.com>
2016-10-21 11:06:13 +0545

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,24 @@
+/*
+Students Name: Rohan Pradhan
+Roll No.:
+Lab No:5
+Subject: Programming Fundmentals
+Program: Odd or even
+Date: 21st october
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+int a;
+printf("enter a number:");
+scanf("%d",&a);
+
+if(a%2==0)
+	printf("%d is a even number",a);
+else
+printf("%d is a odd number",a);
+return 0;
+}
