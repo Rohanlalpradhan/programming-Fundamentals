6161cb57aab6b0a5fef39359a36ff43ee60174bc
Rohanlalpradhan <rohan.pradhan76@gmail.com>
2016-10-21 11:22:09 +0545

    Update greatin3.c

--- greatin3.c
+++ greatin3.c
@@ -17,12 +17,12 @@ int a,b,c;
 printf("Enter 3 numbers a,b&c:");
 scanf("%d%d%d",&a,&b,&c);
 
-if(a>b&&a>c)
+if(a>=b&&a>=c)
 	{
 			printf("%d is greatest number",a);
 	}
 
-else if(b>c&&b>a)
+else if(b>=c&&b>=a)
 	{
 			printf("%d is greatest number",b);
 	}
