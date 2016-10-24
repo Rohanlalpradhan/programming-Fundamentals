ad407b3c45965c4989aeaa3c7ca19d140c76b583
Rohanlalpradhan <rohan.pradhan76@gmail.com>
2016-10-24 10:37:45 +0545

    Add files via upload

---
+++ ascii value.c
@@ -0,0 +1,24 @@
+/*
+Students Name: Rohan Pradhan
+Roll No.:
+Lab No:5
+Subject: Programming Fundmentals
+Program: ternary operators
+Date: 24th october
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+char b;
+
+printf("Enter A Char: \n");
+scanf("%c",&b);
+
+printf("ASCII value of %c is %d",b,b);
+
+
+return 0;
+}
