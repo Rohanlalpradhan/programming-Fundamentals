9bd78fca2acec877ace97225ee56e702f377a695
Rohanlalpradhan <rohan.pradhan76@gmail.com>
2016-12-29 08:35:27 +0545

    Add files via upload

---
+++ table.c
@@ -0,0 +1,23 @@
+/*
+	Students name:Rohan Pradhan
+	Subject:Programming Fundamentals
+	Roll no:20
+	Program: Multipication table
+*/
+#include<stdio.h>
+int main()
+{
+int n,i,j;
+printf("Enter a number:");
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+	{
+			for(j=1;j<=10;j++)
+				{
+					printf("%d\t",i*j);
+				}
+				printf("\n");
+	}
+return 0;
+}
